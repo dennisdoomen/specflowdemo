5a31f9b38dd29a3a96c26408e6b22bb6743d328f
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2012-07-27 14:33:11 +0200

    Finished the automation of the "Attempting to host a dinner as a anonymous user" scenario

--- NerdDinner.SmokeTests/Pages/HomePage.cs
+++ NerdDinner.SmokeTests/Pages/HomePage.cs
@@ -1,4 +1,3 @@
-using System;
 using WatiN.Core;
 using WatiN.Extensions;
 
@@ -6,17 +5,40 @@ namespace NerdDinner.SmokeTests.Pages
 {
     internal class HomePage : Page
     {
+        /// <summary>
+        /// Verifies that the document represents a Url that matches the page metadata.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The default implementation uses information from the associated <see cref="T:WatiN.Core.PageMetadata"/>
+        ///             to validate the <paramref name="url"/>.
+        /// </para>
+        /// <para>
+        /// Subclasses can override this method to customize how document Url verification takes place.
+        /// </para>
+        /// </remarks>
+        /// <param name="url">The document url to verify, not null</param><param name="errorReporter">The error reporter to invoke is the document's properties fail verification</param>
         protected override void VerifyDocumentUrl(string url, ErrorReporter errorReporter)
         {
             if (!url.EndsWith("/"))
             {
-                errorReporter("Couldn't find the homepage");
+                errorReporter("This isn't the home page");
             }
         }
 
         public void LogOff()
         {
-            var div = Document.GetByText<Div>("Log On");
+            Link logOffLink = Document.Link(Find.ByUrl(url => url.ToLower().Contains("logoff")));
+            if (logOffLink.Exists)
+            {
+                logOffLink.Click();
+            }
+        }
+
+        public void HostDinner()
+        {
+            Link hostDinner = Document.Get<Link>(Find.ByUrl(url => url.ToLower().EndsWith("create")));
+            hostDinner.Click(); 
         }
     }
 }
--- NerdDinner.SmokeTests/StepDefinitions/StepDefinitions.cs
+++ NerdDinner.SmokeTests/StepDefinitions/StepDefinitions.cs
@@ -1,28 +1,35 @@
 ﻿using System.Configuration;
 using NerdDinner.SmokeTests.Pages;
 using TechTalk.SpecFlow;
+using FluentAssertions;
 
 namespace NerdDinner.SmokeTests.StepDefinitions
 {
     [Binding]
     public class StepDefinitions : WatiNStepDefinitions
     {
+        [Before]
+        public void GoToHomePage()
+        {
+            Browser.GoTo(ConfigurationManager.AppSettings["SpecFlowRootUrl"]);
+        }
+
         [Given(@"an anonymous user")]
         public void GivenAnAnonymousUser()
         {
-            Browser.GoTo(ConfigurationSettings.AppSettings["SpecFlowRootUrl"]);
             Browser.Page<HomePage>().LogOff();
         }
 
         [When(@"you want to host a dinner")]
         public void WhenYouWantToHostADinner()
         {
+            Browser.Page<HomePage>().HostDinner();
         }
 
         [Then(@"you should be required to log on first")]
         public void ThenYouShouldBeRequiredToLogOnFirst()
         {
-            
+            Browser.Url.Should().Contain("/LogOn");
         }
     }
 }
